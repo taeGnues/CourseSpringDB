56fa49d7d0a4ab2b61d61ee732e73547be138824
정태승 <hi563@g.skku.edu>
2023-06-19 22:19:49 +0900

    Spring JDBC - DELETE/SELECT 사용하기

--- src/main/java/com/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ src/main/java/com/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -1,5 +1,6 @@
 package com.springboot.learnjpaandhibernate.course.jdbc;
 
+import com.springboot.learnjpaandhibernate.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,13 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert();
+        repository.insert(new Course(1, "Learn AWS", "in29minutes"));
+        repository.insert(new Course(2, "Learn Azure", "in29minutes"));
+        repository.insert(new Course(3, "Learn Devops", "in29minutes"));
+
+        repository.deleteById(1);
+
+        System.out.println(repository.findById(2));
+        System.out.println(repository.findById(3));
     } // Spring 시작시 실행됨.
 }
--- src/main/java/com/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
+++ src/main/java/com/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
@@ -1,6 +1,8 @@
 package com.springboot.learnjpaandhibernate.course.jdbc;
 
+import com.springboot.learnjpaandhibernate.Course;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -11,9 +13,32 @@ public class CourseJdbcRepository {
     private static String INSERT_QUERY = // JdbcTemplate으로부터 쿼리문 작성
             """
                insert into course(id,name,author) 
-               values(1,'Learn AWS', 'in28minutes');
-           """;
-    public void insert(){
-        springJdbcTemplate.batchUpdate(INSERT_QUERY);
+               values(?,?,?);
+           """; // 이렇게 ???를 넣으면 해당 부분에 값이 들어감.
+
+    private static String DELTE_QUERY = // JdbcTemplate으로부터 쿼리문 작성
+            """
+               delete from course
+               where id=?
+           """; // 이렇게 ???를 넣으면 해당 부분에 값이
+
+    private static String SELECT_QUERY =
+            """
+                select * from course
+                where id = ?
+            """;
+    public void insert(Course course){
+        springJdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(), course.getAuthor());
+    }
+
+    public void deleteById(long id){ // 삭제 구현
+        springJdbcTemplate.update(DELTE_QUERY, id);
+    }
+
+    public Course findById(long id){ // 삭제 구현
+        return springJdbcTemplate.queryForObject(SELECT_QUERY, new BeanPropertyRowMapper<>(Course.class), id);
+        // select 연산으로 얻은 결과를 Object로 만듦.
+        // select로 연산 결과로 얻은 결과 세트(객체)를 Bean에 매핑하는 과정이 필요함
+        // Bean => Row Mapper로 이어줌.
     }
 }
